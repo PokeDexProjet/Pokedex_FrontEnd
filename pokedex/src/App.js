ddf88d530cb0a39f53aeeaf5de3afc47099a51ef
HuangjingL <151631854+Bonjoureloi@users.noreply.github.com>
2025-01-08 15:04:16 +0000

    all front

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,31 +1,13 @@
-// src/App.js
-import React, { useEffect, useState } from 'react';
-import Pokemon from './components/Pokemon';  // citer le component Pokemon
-import './App.css';
+import React from 'react';
+import Home from './pages/Home';  // 引入首页组件
+import './App.css';  // 引入样式文件
 
 const App = () => {
-  const [pokemons, setPokemons] = useState([]);
-
-  useEffect(() => {
-    const fetchPokemons = async () => {
-      const response = await fetch('http://localhost:8080/pokemons');  // 从后端 API 获取宝可梦数据
-      const data = await response.json();
-      setPokemons(data);
-    };
-
-    fetchPokemons();
-  }, []);
-
   return (
-    <div className="App">
-      <h1>Pokemon List</h1>
-      <div className="pokemon-container">
-        {pokemons.map((pokemon, index) => (
-          <Pokemon key={index} name={pokemon.name} image={pokemon.image} />
-        ))}
-      </div>
+    <div className="app">
+      <Home />
     </div>
   );
-}
+};
 
 export default App;
